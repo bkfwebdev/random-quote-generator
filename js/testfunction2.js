781fb8c7e0df4ca7f6aac040d984657262008c8a
bkfwebdev <bryant.feld@gmail.com>
2018-03-13 20:23:49 -0400

    added test functions

--- js/testfunction2.js
+++ js/testfunction2.js
@@ -1,7 +1,7 @@
-var targetURL = "http://quotes.rest/qod.js";
+var targetURL = "http://quotes.rest/quote/random.json";
 const request = require('request');
  request(targetURL, { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
   console.log(body)
-  
+  // console.log(body.contents.quotes[0].quote);
 });
